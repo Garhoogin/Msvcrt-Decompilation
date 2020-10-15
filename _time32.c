a1fd94daea536c8787ee2fff94cf9c789bab8f56
Garhoogin <declan.c.moore@gmail.com>
2020-10-15 09:08:12 -0500

    Made code more readable

--- _time32.c
+++ _time32.c
@@ -1,14 +1,12 @@
 __time32_t _time32(__time32_t *destTime){
 	long long fileTime;
 	GetSystemTimeAsFileTime((FILETIME *) &fileTime);
-	fileTime -= 0x19DB1DED53E8000;
+	fileTime -= 116444736 * 1000 * 1000 * 1000;
 	fileTime /= 10000000;
 	
 	__time32_t tm = fileTime & 0xFFFFFFFF;
-	if(var8 >= 0){
-		if(var8 > 0x7FFFD27F){
-			tm = -1;
-		}
+	if(fileTime > 0x7FFFD27F){
+		tm = -1;
 	}
 	if(destTime != NULL) *destTime = tm;
 	return tm;
