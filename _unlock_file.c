088ba7b42d69053b066258bc13a3948b8f8b49fd
Garhoogin <declan.c.moore@gmail.com>
2020-10-19 17:37:39 -0500

    Update and rename _unlock_file to _unlock_file.c

--- _unlock_file.c
+++ _unlock_file.c
@@ -1,7 +1,7 @@
 int _unlock(FILE *file){
 	if(file >= _iob && file <= &_iob[19]){
 		file->_flag &= ~0x8000;
-		return _unlock((file - _iob) / sizeof(FILE) + 0x10);
+		return _unlock((file - _iob) + 0x10);
 	}
 	//not exactly sure what this is for
 	return LeaveCriticalSection((LPCRITICAL_SECTION) (file + 1));
